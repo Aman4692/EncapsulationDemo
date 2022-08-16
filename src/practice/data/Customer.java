a67e3cd31ceab7be77f773a564aa35d48bc94c1c
Aman Gupta <aman.gupta4692@gmail.com>
2022-08-16 13:27:37 +0530

    Practice mid commit

--- src/practice/data/Customer.java
+++ src/practice/data/Customer.java
@@ -2,5 +2,51 @@ package practice.data;
 
 public class Customer {
 
+    private String name;
+    private String address;
+    private String email;
+    private long phone;
+    private boolean isPremiumMember;
+
+    public void setIsPremiumMember(boolean premiumMember) {
+        isPremiumMember = premiumMember;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public boolean isPremiumMember() {
+        return isPremiumMember;
+    }
+
 
 }
